b973e59fde05d34a0a1d99ff7e550e11f06f46df
monster57 <surajitbarman57@yahoo.in>
2016-07-27 18:50:51 +0530

    added order and container before running the tests and deleting all the variable after test run

--- test/OrderContainer_test.js
+++ test/OrderContainer_test.js
@@ -1,5 +1,6 @@
 var assert = require('chai').assert;
 var OrderContainer = require('../app/OrderContainer');
+var Order = require('../app/Order');
 
 
 describe('OrderContainer', function() {
@@ -18,5 +19,24 @@ describe('OrderContainer', function() {
     	it('should give emptyContainer', function () {
 	      assert.deepEqual(this.orderContainer.getContainer(), this.emptyContainer);
 	    });
+    })
+
+    describe('after adding Order', function(){
+    	afterEach(function(){
+    		delete order;
+	       	delete companyName;
+	       	delete stockId;
+	       	delete quantity;
+	       	delete side;
+    	});
+
+    	before(function(){
+    		this.companyName = "ABC";
+	  		this.stockId = 1;
+	  		this.side = "buy";
+	  		this.quantity = 20;
+	        this.order = new Order(this.stockId, this.side, this.companyName, this.quantity);
+    		this.orderContainer = new OrderContainer();
+    	});
     })
 });
