59f43384e15f8f4568526cede66b2a03be2fb06d
monster57 <surajitbarman57@yahoo.in>
2016-07-27 09:25:50 +0530

    added all test for order object

--- test/Order_test.js
+++ test/Order_test.js
@@ -21,7 +21,18 @@ describe('test', function() {
     describe('order', function(){
     	it('should give the stockId ', function () {
 	      assert.equal(this.order.getStockId() , this.stockId);
-	      // `afterEach` is invoked here, cleaning out `this`
+	    });
+
+	    it('should give the side ', function () {
+	      assert.equal(this.order.getSide() , this.side );
+	    });
+
+	    it('should give the company name ', function () {
+	      assert.equal(this.order.getCompanyName() , this.companyName);
+	    });
+
+	    it('should give the quantity of item ', function () {
+	      assert.equal(this.order.getQuantity() , this.quantity);
 	    });
     })
 });
